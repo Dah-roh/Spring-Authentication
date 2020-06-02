88cac32e99d8b2567bfcd74a1d47b8bc396594a3
dizsart <darogadibia@gmail.com>
2020-06-02 14:07:55 +0100

    add userEntity in the file structure of the project

--- src/main/java/com/jwt/spring/Application.java
+++ src/main/java/com/jwt/spring/Application.java
@@ -2,12 +2,21 @@ package com.jwt.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @RequestMapping(value = "/products")
+    public String getProductName() {
+        return "Honey";
+    }
 }
