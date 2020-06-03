43eea9205af700947c3bb1a676d55f970bbf15a2
dizsart <darogadibia@gmail.com>
2020-06-03 20:40:49 +0100

    fixed line 66 in JwtTokenUtil file

--- src/main/java/com/jwt/spring/config/JwtTokenUtil.java
+++ src/main/java/com/jwt/spring/config/JwtTokenUtil.java
@@ -63,7 +63,7 @@ public class JwtTokenUtil implements Serializable {
     //validate token
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token));
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     //generate token for user
