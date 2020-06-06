30cf5d3d35f87c472d898dc2d933295986823402
dizsart <darogadibia@gmail.com>
2020-06-06 13:53:19 +0100

    add jwtRequestFilter and jwtAuthenticationEntryPoint to the config package

---
+++ src/main/java/com/jwt/spring/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,4 @@
+package com.jwt.spring.config;
+
+public class JwtAuthenticationEntryPoint {
+}
--- src/main/java/com/jwt/spring/controller/JwtAuthenticationController.java
+++ src/main/java/com/jwt/spring/controller/JwtAuthenticationController.java
@@ -2,10 +2,15 @@ package com.jwt.spring.controller;
 
 
 import com.jwt.spring.config.JwtTokenUtil;
+import com.jwt.spring.model.JwtRequest;
+import com.jwt.spring.model.JwtResponse;
 import com.jwt.spring.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,10 +29,24 @@ public class JwtAuthenticationController {
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
-        authenticat(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
         final UserDetails userDetails = userDetailsService
                 .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        }catch (DisabledException e) {
+            throw new Exception("USER_DENIED", e);
+        } catch(BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
 
 }
+
+
